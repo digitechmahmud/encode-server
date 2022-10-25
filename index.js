7afb517a7dcc1fe65efbbf6473c8f4099b21621d
digitechmahmud <mdmahmudulhassanbd@gmail.com>
2022-10-25 10:00:44 +0600

    Index file initially configured

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send('Successfully server running');
+})
+
+app.listen(port, () => {
+    console.log('app lisenting from port', port);
+})
