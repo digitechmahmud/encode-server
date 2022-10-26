3db9bd0fcaa783e47d290695ea21466e278e2c96
digitechmahmud <mdmahmudulhassanbd@gmail.com>
2022-10-27 00:15:19 +0600

    gitignore create

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const coursesData = require('./data/courses.json');
+const course = require('./data/course.json');
+const checkoutData = require('./data/checkout.json');
 
 app.use(cors());
 
@@ -15,6 +17,16 @@ app.get('/courses', (req, res) => {
     res.send(coursesData);
 })
 
+app.get('/course', (req, res) => {
+    res.send(course);
+})
+
+app.get('/course/:id', (req, res) => {
+    const id = req.params.id;
+    const coursedata = course.find(cd => cd.id === id);
+    res.send(coursedata);
+})
+
 app.get('/courses/:id', (req, res) => {
     const id = req.params.id;
     const singleCourse = coursesData.find(c => c.id === id);
@@ -23,6 +35,13 @@ app.get('/courses/:id', (req, res) => {
     }
     res.send(singleCourse);
 })
+
+app.get('/checkout/:id', (req, res) => {
+    const id = req.params.id; 
+    const singleCheckout = checkoutData.find(cd => cd._id === id);
+    res.send(singleCheckout);
+})
+
 
 app.listen(port, () => {
     console.log('app listening from port', port);
